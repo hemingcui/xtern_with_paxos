d2bce375662d7b09ca6f3f9750a1fd4126ddbeae
heming <heming@cs.hku.hk>
2015-07-07 22:22:43 +0800

    added wrap-xtern.sh script

--- scripts/wrap-xtern.sh
+++ scripts/wrap-xtern.sh
@@ -2,4 +2,4 @@
 # Heming: this script wrap xtern's ld-preload library and faithfully execute it
 # with the full command line from this script. This script is for valgrind to execute with xtern.
 
-LD_PRELOAD=$XTERN_ROOT/dync_hook/interspose.so $*
+LD_PRELOAD=$XTERN_ROOT/dync_hook/interpose.so $*
