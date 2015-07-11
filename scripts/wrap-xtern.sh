f54a534d94c6991ffb61be3d328699d1b09b9286
heming@cs.columbia.edu <heming@cs.columbia.edu>
2015-07-11 05:02:21 -0400

    added more tests

--- scripts/wrap-xtern.sh
+++ scripts/wrap-xtern.sh
@@ -2,4 +2,5 @@
 # Heming: this script wrap xtern's ld-preload library and faithfully execute it
 # with the full command line from this script. This script is for valgrind to execute with xtern.
 
-LD_PRELOAD=$XTERN_ROOT/dync_hook/interpose.so $*
+#LD_PRELOAD=$XTERN_ROOT/dync_hook/interpose.so $*
+LD_PRELOAD=/home/heming/hku/m-smr/xtern/dync_hook/interpose.so $*
