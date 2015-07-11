8595b889b14fc0a5b6e3701661d7cdb7ba4f484a
heming@cs.columbia.edu <heming@cs.columbia.edu>
2015-07-11 11:09:07 -0400

    added more tests

--- scripts/wrap-xtern.sh
+++ scripts/wrap-xtern.sh
@@ -3,4 +3,4 @@
 # with the full command line from this script. This script is for valgrind to execute with xtern.
 
 BASEDIR=$(dirname $0)
-LD_PRELOAD=$BASEDIR/../dync_hook/interpose.so $*
+LD_PRELOAD=$BASEDIR/../dync_hook/interpose.so eval "$*"
