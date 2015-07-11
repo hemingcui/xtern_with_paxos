6b812e829e29dc81b9532d7906f54cf9dc661ef3
heming <heming@cs.hku.hk>
2015-07-11 17:24:43 +0800

    nits

--- scripts/wrap-xtern.sh
+++ scripts/wrap-xtern.sh
@@ -3,4 +3,4 @@
 # with the full command line from this script. This script is for valgrind to execute with xtern.
 
 #LD_PRELOAD=$XTERN_ROOT/dync_hook/interpose.so $*
-LD_PRELOAD=/home/heming/hku/m-smr/xtern/dync_hook/interpose.so $*
+LD_PRELOAD=$XTERN_ROOT/dync_hook/interpose.so $*
